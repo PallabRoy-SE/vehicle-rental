541a205dc6083b5339ad9f6288c202c933fcd1cc
PALLAB ROY <76547199+PallabRoy-SE@users.noreply.github.com>
2024-05-20 11:35:16 +0530

    init: initialization frontend ( refs #7 )
    - Necessary libraries installed

---
+++ vehicle-rental-app/src/App.js
@@ -0,0 +1,12 @@
+import Typography from "@mui/material/Typography";
+import "./App.css";
+
+function App() {
+  return (
+    <Typography variant="h1" className="App text-3xl font-bold underline">
+      Vehicle rental app
+    </Typography>
+  );
+}
+
+export default App;
